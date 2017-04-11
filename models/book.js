b27c5c8fe1896a1f9a2d49a2bb1331989d03453e
janeosaur <janewie@gmail.com>
2017-04-11 16:18:57 -0700

    completed steps 1-5

---
+++ models/book.js
@@ -0,0 +1,22 @@
+// This will be used to create schema and model for books
+var mongoose = require('mongoose');
+
+// Books will have the following attributes: title, author, image, release_date
+
+// Book Schema
+var Schema = mongoose.Schema;
+
+var BookSchema = new Schema({
+     title: String,
+     author: String,
+     image: String,
+     releaseDate: String
+ });
+
+
+// Create Book Model
+ var Book = mongoose.model('Book', BookSchema);
+
+ 
+ // Export Book from this module
+module.exports = Book;
--- models/index.js
+++ models/index.js
@@ -1,2 +1,7 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/book-app");
+
+// Import each model and export an object called exports
+
+module.exports.Book = require('./book.js');
+// module.exports.Other = require('./other.js'); // to export Others if needed.
